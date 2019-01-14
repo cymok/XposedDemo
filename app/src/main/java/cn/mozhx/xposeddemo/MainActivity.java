c21c54eae43d46e09f344adb3a0bc31f495669ef
Harry <mozhx020@163.com>
2019-01-15 03:52:30 +0800

    检测模块是否激活

--- app/src/main/java/cn/mozhx/xposeddemo/MainActivity.java
+++ app/src/main/java/cn/mozhx/xposeddemo/MainActivity.java
@@ -3,6 +3,8 @@ package cn.mozhx.xposeddemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +13,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        isActivating(false);
+
+    }
+
+    private void isActivating(boolean activating) {
+        ImageView imageView = (ImageView) findViewById(R.id.iv);
+        String text;
+        if (activating) {
+            imageView.setImageResource(R.drawable.qy);
+            text = "activating";
+        } else {
+            imageView.setImageResource(0);
+            text = "no activating";
+        }
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/cn/mozhx/xposeddemo/XposedHook.java
+++ app/src/main/java/cn/mozhx/xposeddemo/XposedHook.java
@@ -19,6 +19,30 @@ public class XposedHook implements IXposedHookLoadPackage {
         String hookPkg = "com.tencent.mm";
         String className = "com.tencent.mm.ui.SplashImageView";
 
+        String packageName = loadPackageParam.packageName;
+        ClassLoader classLoader = loadPackageParam.classLoader;
+
+        //检测模块是否激活 在应用自己的页面
+        if (TextUtils.equals(BuildConfig.APPLICATION_ID, packageName)) {
+            //om.xposed_wechat.xz.MainActivity   需要Hook类名
+            //showIsXposedStart   需要Hook类名下面的方法名
+            XposedBridge.log("xxxxxxxxxxxxxxxxxxxx isActivating:start");
+            XposedHelpers.findAndHookMethod(
+                    "cn.mozhx.xposeddemo.MainActivity",
+                    classLoader,
+                    "isActivating",
+                    boolean.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            //强行改变方法的参数
+                            param.args[0] = true;
+                            XposedBridge.log("xxxxxxxxxxxxxxxxxxxx isActivating:beforeHookedMethod");
+                            super.beforeHookedMethod(param);
+                        }
+                    });
+        }
+
         XposedBridge.log("handleLoadPackage:" + loadPackageParam.packageName);
         if (!TextUtils.equals(hookPkg, loadPackageParam.packageName)) {
             return;
